ad0ffdf117820343b2f216d33ad9c2261b41082f
Paco Muñoz <iicc96@hotmail.com>
2022-06-06 05:16:44 +0200

    finish BeerInventoryController implementation, load data with data.sql and beerInventoryController unit test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Beer_Inventory (beer_Id, upc, quantity_On_Hand) values ('0a818933-087d-47f2-ad83-2f986ed087eb', '0631234200031', 15);
+INSERT INTO Beer_Inventory (beer_Id, upc, quantity_On_Hand) values ('a712d914-61ea-4623-8bd0-32c0f6545bfd', '0631234300012', 19);
+INSERT INTO Beer_Inventory (beer_Id, upc, quantity_On_Hand) values ('026cc3c8-3a0c-4083-a05b-e908048c1b08', '0083783375214', 34);
+
+
---
+++ src/test/java/com/pmg/beerinventoryservice/web/controller/BeerInvetoryControllerTest.java
@@ -0,0 +1,16 @@
+package com.pmg.beerinventoryservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerInvetoryControllerTest {
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void listBeerById() {
+    }
+}
